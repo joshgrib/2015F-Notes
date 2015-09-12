8b56a5158c3c932276cb8b1447ad975b079b6d15
joshgrib <jgribbon@stevens.edu>
2015-09-12 17:18:41 -0400

    Finished 284 hw1

--- cs284/hw1/hw1.java
+++ cs284/hw1/hw1.java
@@ -1,121 +1,90 @@
+/*
+Josh Gribbon
+CS284 - HW1
+I pledge my honor I have abided by the Stevens Honor System.
+
+Note: I didn't need to check for a negative degree or anything like that because it's determined by the length of the array. Any values in the array or fine(assuming they're real numbers).
+*/
+import java.util.Arrays;
 public class hw1{
     public static class PolySeries{
+        //declare variables
         public int degree;
-        public double coef;
-        //Instantiate values
-        public PolySeries(int degree_init, double coef_init){
-            //validate values
-            if(degree_init < 0){
-                System.out.println("Sorry, degree cannot be negative, exiting...");
-                System.exit(1);
+        public double[] coefs;
+        public String series_str;
+
+        //constructor
+        public PolySeries(double[] coefs_init){
+            coefs = coefs_init;
+            //Everything below is just for presentation
+            System.out.println("PolySeries created with coefficients:" + Arrays.toString(coefs));
+            degree = coefs.length;
+            series_str = "P(x)=";
+            for(int i=0; i<degree; i++){
+                if(coefs[degree-(i+1)] != 0)
+                {
+                    if(i!=0)
+                    {
+                        //if the coefficient is zero no need to print out that term
+                        series_str = series_str + " + ";
+                    }
+                    //wanted to reverse the order so it shows the summation starting with the highest degree terms
+                    series_str = series_str + coefs[degree-(i+1)] + "*x^" + (degree-(i+1));
+                }
             }
-            else{
-                degree = degree_init;
-            }
-            // coefficient can be any double
-            coef = coef_init;
-            System.out.println("PolySeries created with coefficient:" + coef + " and degree:" + degree);
+            System.out.println(series_str);
         }
-        //Given x, return the series
+
+        //Given x, return the value of the summation
         public double P(double x){
             System.out.println("PolySeries called as P(" + x + ")");
-            //loop through the series
             double result = 0;
-            for(int i=0; i<(degree + 1); i++){
-                double newResult = result + (coef * Math.pow(x,i));
-                System.out.println(result + "  +  " + coef + "*" + x + "^" + i + "=" + newResult);
+            for(int i=0; i<degree; i++){
+                double newResult = result + (coefs[i] * Math.pow(x,i));
                 result = newResult;
             }
             return result;
         }
     }
+
     public static void main(String[] args) {
         System.out.println("---");
-        PolySeries seriesOne = new PolySeries(3,1);
-        double result1 = seriesOne.P(2);
-        System.out.println("Result:" + result1);
-        PolySeries seriesTwo = new PolySeries(4,-2.1);
-        double result2 = seriesTwo.P(4);
-        System.out.println("Result:" + result2);
-        System.out.println("Addition:");
-        System.out.println(result2 + "+" + result1 + "=" + (result1 + result2));
+
+        //Polynomial 1
+        double[] input1 = {5.4, 7, 11.1, -17, 1.3};
+        PolySeries series1 = new PolySeries(input1);
+        System.out.println("Result:" + series1.P(2));
+
+        System.out.println();
+
+        //Polynomial 2
+        double[] input2 = {0, 0, 4, 6.7, -1.2};
+        PolySeries series2 = new PolySeries(input2);
+        System.out.println("Result:" + series2.P(4));
+
+        System.out.println();
+
+        //Adding the polynomials
+        double seriesAdded = series1.P(2) + series2.P(4);
+        System.out.println("Sum: " + seriesAdded);
+
         System.out.println("---");
-        System.out.println("And now an error on purpose:");
-        PolySeries seriesError = new PolySeries(-2,8.7);
-        double resultError = seriesError.P(3);
     }
 }
+/* TERMINAL OUTPUT
+---
+PolySeries created with coefficients:[5.4, 7.0, 11.1, -17.0, 1.3]
+P(x)=1.3*x^4 + -17.0*x^3 + 11.1*x^2 + 7.0*x^1 + 5.4*x^0
+PolySeries called as P(2.0)
+Result:-51.400000000000006
 
-/*TERMINAL OUTPUT
-    ---
-    PolySeries created with coefficient:1.0 and degree:3
-    PolySeries called as P(2.0)
-    0.0  +  1.0*2.0^0=1.0
-    1.0  +  1.0*2.0^1=3.0
-    3.0  +  1.0*2.0^2=7.0
-    7.0  +  1.0*2.0^3=15.0
-    Result:15.0
-    PolySeries created with coefficient:-2.1 and degree:4
-    PolySeries called as P(4.0)
-    0.0  +  -2.1*4.0^0=-2.1
-    -2.1  +  -2.1*4.0^1=-10.5
-    -10.5  +  -2.1*4.0^2=-44.1
-    -44.1  +  -2.1*4.0^3=-178.5
-    -178.5  +  -2.1*4.0^4=-716.1
-    Result:-716.1
-    Addition:
-    -716.1+15.0=-701.1
-    ---
-    And now an error on purpose:
-    Sorry, degree cannot be negative, exiting...
-*/
+PolySeries created with coefficients:[0.0, 0.0, 4.0, 6.7, -1.2]
+P(x)=-1.2*x^4 + 6.7*x^3 + 4.0*x^2
+PolySeries called as P(4.0)
+Result:185.60000000000002
 
-/*
-http://rosettacode.org/wiki/Horner's_rule_for_polynomial_evaluation
-
-a_i is an array s.t. a polynomial P(x) can be defined as:
-[12, 3, -4, 0, 1, 0, 4]
-P(x) = 4x^6 + x^4 - 4x^2 + 3x + 12
-
-The class should take in the array, the degree would be the length of the array, and the coefficients would be the awway values. Each term would be:
-    a[i] * x^i
-
-Two polynomials could be added(without giving x values first) by adding the array terms together. There would probably need to be something for arrays of different lengths.
-*/
-
-/* PYTHON - Horner's method of adding polynomials
->>> def horner(coeffs, x):
-    acc = 0
-    for c in reversed(coeffs):
-        acc = acc * x + c
-    return acc
-
->>> horner( (-19, 7, -4, 6), 3)
-128
-*/
-
-/* JAVA - Horner's method of adding polynomials
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class Horner {
-    public static void main(String[] args){
-        List<Double> coeffs = new ArrayList<Double>();
-        coeffs.add(-19.0);
-        coeffs.add(7.0);
-        coeffs.add(-4.0);
-        coeffs.add(6.0);
-        System.out.println(polyEval(coeffs, 3));
-    }
-
-    public static double polyEval(List<Double> coefficients, double x) {
-        Collections.reverse(coefficients);
-        Double accumulator = coefficients.get(0);
-        for (int i = 1; i < coefficients.size(); i++) {
-            accumulator = (accumulator * x) + (Double) coefficients.get(i);
-        }
-        return accumulator;
-    }
-}
+PolySeries called as P(2.0)
+PolySeries called as P(4.0)
+Sum: 134.20000000000002
+---
 */
